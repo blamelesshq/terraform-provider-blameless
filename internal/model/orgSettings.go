82a9ac466f97330eb34a76779a16322f0c4cd49c
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2023-08-16 08:31:43 -0500

    INC-656 adding json definitions to model

--- internal/model/orgSettings.go
+++ internal/model/orgSettings.go
@@ -1,14 +1,14 @@
 package model
 
 type OrgSettings struct {
-	Name          string
-	Timezone      string
-	Description   string
-	IncidentRoles []string
-	Severities    []*IncidentSeverity
+	Name          string              `json:"name"`
+	Timezone      string              `json:"timezone"`
+	Description   string              `json:"description"`
+	IncidentRoles []string            `json:"incidentRoles"`
+	Severities    []*IncidentSeverity `json:"severities"`
 }
 
 type IncidentSeverity struct {
-	Level int
-	Label string
+	Level int    `json:"level"`
+	Label string `json:"label"`
 }
