8e0ce26ec4875ee64d0436b13066393b1eaa6f71
Dillon Adams <28228453+DillonAd@users.noreply.github.com>
2023-09-27 13:21:36 -0500

    INC-710 Refactoring Incident Severity format

--- internal/resource/incidentseverities/expand.go
+++ internal/resource/incidentseverities/expand.go
@@ -8,7 +8,7 @@ import (
 
 func expandSettings(config cty.Value) *model.IncidentSeveritySettings {
 	settings := &model.IncidentSeveritySettings{
-		Severities: expandIncidentSeverties(config.GetAttr("severities")),
+		Severities: expandIncidentSeverties(config.GetAttr("severity")),
 	}
 	return settings
 }
@@ -16,24 +16,10 @@ func expandSettings(config cty.Value) *model.IncidentSeveritySettings {
 func expandIncidentSeverties(severities cty.Value) []*model.IncidentSeverity {
 	var results []*model.IncidentSeverity
 	severities.ForEachElement(func(_, sev cty.Value) (stop bool) {
-		results = []*model.IncidentSeverity{
-			{
-				Level: 0,
-				Label: *value.String(sev.GetAttr("sev0_label")),
-			},
-			{
-				Level: 1,
-				Label: *value.String(sev.GetAttr("sev1_label")),
-			},
-			{
-				Level: 2,
-				Label: *value.String(sev.GetAttr("sev2_label")),
-			},
-			{
-				Level: 3,
-				Label: *value.String(sev.GetAttr("sev3_label")),
-			},
-		}
+		results = append(results, &model.IncidentSeverity{
+			Level: *value.Int(sev.GetAttr("level")),
+			Label: *value.String(sev.GetAttr("label")),
+		})
 		return stop
 	})
 
